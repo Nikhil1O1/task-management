9c6012a81d6e6ebf1405cf7db916ba469b8d51db
Nikhil1O1 <technikue2.0@gmail.com>
2024-05-11 02:57:21 +0530

    worked on font and logo, wip

--- frontend/app/(marketing)/page.tsx
+++ frontend/app/(marketing)/page.tsx
@@ -1,14 +1,24 @@
 import { Button } from '@/components/ui/button'
+import {cn} from '@/lib/utils'
 import {  Medal } from 'lucide-react'
+import { Poppins } from 'next/font/google'
+import localFont from 'next/font/local'
 import Link from 'next/link'
 import React from 'react'
 
 type Props = {}
 
+const headingFont = localFont({src:'../../public/fonts/CalSans-SemiBold.woff2'})
+const textFont = Poppins({
+    subsets: ['latin'],
+    weight: ['100', '200', '300','400', '500', '600', '700','800','900'],
+})
+
 const MarketingPage = (props: Props) => {
   return (
     <div className='flex items-center justify-center flex-col'>
-        <div className='flex items-center justify-center flex-col'>
+        <div className={cn('flex items-center justify-center flex-col',
+        headingFont.className,)}>
             <div className='mb-4 flex items-center border shadow-sm p-4
             bg-amber-100 text-amber-700 rounded-full uppercase'>
                 <Medal className='h-6 w-6 mr-2'/>
@@ -22,14 +32,17 @@ const MarketingPage = (props: Props) => {
                 keep working
             </div>
         </div>
-        <div className='text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto' >
+        <div className={cn(
+            `text-sm md:text-xl text-neutral-400 mt-4 
+        max-w-xs md:max-w-2xl text-center mx-auto`,
+         textFont.className)} >
             Collaborate, Manage and be a TeamPlayer.
             <br/>
                    Per Aspera ad Astra!
         </div>
-        <Button>
+        <Button className='mt-6' size="lg" asChild >
             <Link href="/sign-up">
-                Let's go
+                Lets go
             </Link>
         </Button>
     </div>
