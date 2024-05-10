b4a188e31dd64d7bc347ebb91218d3b7972dcde5
Nikhil <164010411+nikhilJcasp@users.noreply.github.com>
2024-05-10 19:50:55 +0530

    initial commit 55:00, frontend landing page

---
+++ frontend/app/(marketing)/layout.tsx
@@ -0,0 +1,17 @@
+const MarketingPage = ({
+    children
+}:{
+    children: React.ReactNode;
+})=> {
+    return(
+        <div className="h-full bg-slate-200">
+            {/* navbar */}
+            <main className="pt-40 pb-20 bg-slate-200">
+                {children}
+            </main>
+            {/* footer */}
+        </div>
+    )
+}
+
+export default MarketingPage
