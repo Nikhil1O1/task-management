c2e349a3d99f52b0943ae6e44dd33fb2fee4f3c3
Nikhil <164010411+nikhilJcasp@users.noreply.github.com>
2024-05-11 12:58:43 +0530

    completed landing page ui

--- frontend/components/Logo.tsx
+++ frontend/components/Logo.tsx
@@ -4,12 +4,12 @@ import Image from "next/image"
 import { cn } from "@/lib/utils"
 import localFont from "next/font/local"
 
-const headingFont = localFont({src:'../../public/fonts/CalSans-SemiBold.woff2'})
+const headingFont = localFont({src:'../public/fonts/CalSans-SemiBold.woff2'})
 
 export const Logo = () => {
     return(
         <Link href="/">
-            <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
+            <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex text-white">
                 <Image src="/logo.svg" width={30} height={30} alt="logo" />
                 <p className={cn("text-lg text-neutral-700 pb-1",
                     headingFont.className,
